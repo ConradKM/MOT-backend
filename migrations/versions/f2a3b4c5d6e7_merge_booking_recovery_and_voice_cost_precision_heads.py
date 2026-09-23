d831ad8e8eaea41dcbbeeac38787ef87f57ef31f
Mahin558 <mahinshamaun@gmail.com>
2026-09-24 00:38:27 +0100

    Merge current migration heads

---
+++ migrations/versions/f2a3b4c5d6e7_merge_booking_recovery_and_voice_cost_precision_heads.py
@@ -0,0 +1,19 @@
+"""Merge booking recovery and voice-cost precision migration heads.
+
+Revision ID: f2a3b4c5d6e7
+Revises: f1a2b3c4d5e6, d4e5f6a7b8c9
+Create Date: 2026-09-24
+"""
+
+revision = "f2a3b4c5d6e7"
+down_revision = ("f1a2b3c4d5e6", "d4e5f6a7b8c9")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
